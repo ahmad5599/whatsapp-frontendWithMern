ee38f8e870a643afa3c6d8c17d3e78c766291dad
Ahmad Hamid <ahmad.hamid599@outlook.com>
2022-07-23 22:12:56 +0500

    chat area complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,47 @@
 import React from "react";
 import "./Sidebar.css";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import { Avatar, IconButton } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
+        <Avatar src="https://cdn140.picsart.com/298405491215211.png" />
         <div className="sidebar__headerRight">
-          <DonutLargeIcon />
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-        <div className="sidebar__headerAvatar"></div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <div className="sidebar__chatsRows">
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+        </div>
       </div>
     </div>
   );
