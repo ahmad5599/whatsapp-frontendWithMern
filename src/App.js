49757e50626be23d45b8180e3002777ad6b4617a
Ahmad Hamid <ahmad.hamid599@outlook.com>
2022-07-25 00:29:46 +0500

    completed db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("3837ddb7118dbb2fa7f5", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       {/* <h1>༼ つ ◕_◕ ༽つLets gooo!!!(╯°□°）╯︵ ┻━┻</h1> */}
